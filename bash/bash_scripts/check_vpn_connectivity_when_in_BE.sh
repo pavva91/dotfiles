8a4f49bbe04a3e45fef5b612287c7c9bdfab6372
pavva91 <valerio.mattioli@protonmail.com>
2022-11-10 18:53:51 +0100

    Add VPN Script

---
+++ bash/bash_scripts/check_vpn_connectivity_when_in_BE.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ISP=$(curl -s ifconfig.co/json | awk -F "\"" '/asn_org/ {print $4}')
+country_iso=$(curl -s ifconfig.co/json | awk -F "\"" '/country_iso/ {print $4}')
+vpn_connected=0 # Defaults to false
+
+if [ "$country_iso" != "BE" ]; then
+    if [ "$ISP" != "Orange Belgium SA" ]; then
+        vpn_connected=1
+    fi
+elif [ "$ISP" != "Orange Belgium SA" ]; then
+    vpn_connected=1
+fi
+
+if [ $vpn_connected -eq 1 ]; then
+    echo "VPN Connected, Country: $country_iso and Organization: $ISP"
+else
+    echo "VPN NOT Connected, Country: $country_iso and Organization: $ISP"
+fi
+
---
+++ bash/bash_scripts/start_vpn.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+check_if_vpn_name_correct () {
+    valid_input=$(sudo ls /etc/openvpn/client | awk -F '[-.]' '/-/ { print $1"-"$2"-"$3"\n" }' | grep "^$1$" | wc -l)
+    if [[ $valid_input != 1 ]]; then
+        echo "Invalid VPN Input: $1"
+        exit 0
+    else
+        echo "Available VPN: $1"
+    fi
+}
+
+list_available_VPNs () {
+    echo "$(sudo ls /etc/openvpn/client | awk -F '[-.]' '/-/ { print $1"-"$2"-"$3", " }')"
+}
+
+# while getopts 'la:' OPTION; do
+while getopts "lc:" OPTION; do
+    case $OPTION in
+        l)
+            list_available_VPNs
+            exit 0
+            ;;
+        c)
+            check_if_vpn_name_correct $2
+            exit 0
+            ;;
+        ?)
+            echo "Script usage: -l to get the list of available VPNs."
+            exit 0
+            ;;
+    esac
+done
+
+echo "Please select VPN connection:"
+echo "$(list_available_VPNs)"
+read -p 'Insert VPN: ' vpn
+
+check_if_vpn_name_correct $vpn
+
+already_active_VPN=$(systemctl status | grep '\.protonvpn\.net\.udp\.service$' | awk -F "@" '{ print $2  }')
+echo "Disconnect from $already_active_VPN"
+sudo systemctl stop openvpn-client@$already_active_VPN
+sudo systemctl disable openvpn-client@$already_active_VPN
+
+echo "Connect to: $vpn"
+sudo systemctl start openvpn-client@$vpn.protonvpn.net.udp.service
+connected_VPN=$(systemctl status | grep '\.protonvpn\.net\.udp\.service$' | awk -F "@" '{ print $2  }')
+echo "Connected to: $connected_VPN"
+
+sleep 5
+~/bash_scripts/check_vpn_connectivity_when_in_BE.sh
