1c65e2c841537ef66ee867ea72c3fce74c078317
pavva91 <valerio.mattioli@protonmail.com>
2022-11-12 14:59:19 +0100

    Modified grep

--- bash/bash_scripts/vpn_fix_after_inactivity_wifi.sh
+++ bash/bash_scripts/vpn_fix_after_inactivity_wifi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # When system goes inactive (e.g. close the laptop) the WiFi connection is disrupted, dhcpd will reset the default gateway to its old value. Check if the rerouting to VPN is still there, if not restart VPN
 # link: https://bbs.archlinux.org/viewtopic.php?id=274242
-is_vpn_routing=$(ip route | grep "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+ via 192.168.0.1 dev wlan0" | wc -l)
+is_vpn_routing=$(ip route | grep "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+ via 192.168.[0-9]\+\.[0-9]\+ dev wlan0" | wc -l)
 if [[ $is_vpn_routing == 0 ]]; then
     echo "VPN connection dropped, should restart openvpn client"
     already_active_VPN=$(systemctl status | grep '\.protonvpn\.net\.udp\.service$' | awk -F "@" '{ print $2  }')
