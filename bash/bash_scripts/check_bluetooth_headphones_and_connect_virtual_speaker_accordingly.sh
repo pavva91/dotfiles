a569d460060c8df10e3b793c4a719ea466211969
pavva91 <valerio.mattioli@protonmail.com>
2022-11-18 16:32:06 +0100

    Add bash script to check if bluetooth headset is present, if so, connect Virtual Speaker output to bluetooth headset, if not present, connect Virtual Speaker output to Built-In Audio

---
+++ bash/bash_scripts/check_bluetooth_headphones_and_connect_virtual_speaker_accordingly.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+previous_bluetooth_outputs=$(<~/log_bash_scripts/bluetooth_outputs.log)
+bluetooth_outputs=$(pw-link --output | rg "bluez_output" | wc -l)
+
+echo $bluetooth_outputs > ~/log_bash_scripts/bluetooth_outputs.log
+echo "Previous value: $previous_bluetooth_outputs"
+echo "Actual value: $bluetooth_outputs"
+
+if [[ $bluetooth_outputs != $previous_bluetooth_outputs ]]; then
+    if [[ $bluetooth_outputs == 0 ]]; then
+        # Connect Gain Virtual Speaker to Built-In Audio
+        pw-link "Gain Virtual Speaker (Stereo):Audio Output 1" alsa_output.pci-0000_00_1b.0.playback.0.0:playback_FL
+        pw-link "Gain Virtual Speaker (Stereo):Audio Output 2" alsa_output.pci-0000_00_1b.0.playback.0.0:playback_FR
+    else
+        # Disconnect Gain Virtual Speaker to Built-In Audio
+        pw-link --disconnect "Gain Virtual Speaker (Stereo):Audio Output 1" alsa_output.pci-0000_00_1b.0.playback.0.0:playback_FL
+        pw-link --disconnect "Gain Virtual Speaker (Stereo):Audio Output 2" alsa_output.pci-0000_00_1b.0.playback.0.0:playback_FR
+
+        # Connect Gain Virtual Speaker to bluetooth WH-1000XM3
+        pw-link "Gain Virtual Speaker (Stereo):Audio Output 1" bluez_output.38_18_4C_18_C8_2D.1:playback_FL
+        pw-link "Gain Virtual Speaker (Stereo):Audio Output 2" bluez_output.38_18_4C_18_C8_2D.1:playback_FR
+    fi
+fi
