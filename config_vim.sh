8fa1d43550984ee74a05b7408705e095342aac17
pavva91 <valerio.mattioli@protonmail.com>
2023-02-08 10:13:21 +0100

    Add separate vim configuration script

---
+++ config_vim.sh
@@ -0,0 +1,8 @@
+# Install Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Dependencies of CoC
+sudo pacman -S /
+    nodejs
+    npm
