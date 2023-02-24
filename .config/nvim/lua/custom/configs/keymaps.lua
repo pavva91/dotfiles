254968d191b46c65076d8c31ebea635611615c0f
pavva91 <valerio.mattioli@protonmail.com>
2023-02-24 14:29:51 +0100

    Add Buffer List Open in Vertical Split <leader>b

--- .config/nvim/lua/custom/configs/keymaps.lua
+++ .config/nvim/lua/custom/configs/keymaps.lua
@@ -66,3 +66,11 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- buffer search shortcut
+vim.keymap.set('n', '<leader>B', '<cmd>ls<cr>:b<space>',
+	{ desc = 'Search on List [B]Buffer' })
+
+-- buffer search shortcut and open in vertical split
+vim.keymap.set('n', '<leader>b', '<cmd>ls<cr>:vert sb<space>',
+	{ desc = 'Search on List [b]Buffer and open on Vertical Split' })
