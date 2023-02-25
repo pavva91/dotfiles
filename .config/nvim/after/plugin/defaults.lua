cd1914779b9af76d701763a5c407329b8926a061
pavva91 <valerio.mattioli@protonmail.com>
2023-02-25 11:57:02 +0100

    Add Treehopper (keymap: m in visual mode, so "vm")

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -8,3 +8,7 @@ vim.keymap.set('n', '<F3>', "<cmd>NeoTreeShowToggle<cr>", { desc = 'Open File Sy
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+
+-- Treehopper
+vim.cmd [[omap     <silent> m :<C-U>lua require('tsht').nodes()<CR>]]
+vim.cmd [[vnoremap <silent> m :lua require('tsht').nodes()<CR>]]
