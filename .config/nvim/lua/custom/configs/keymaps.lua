429e35b13f3f41d01531b0a3369f520238039725
pavva91 <valerio.mattioli@protonmail.com>
2023-03-07 17:29:49 +0100

    Integrated LSP keymaps with LSP Saga keymaps

--- .config/nvim/lua/custom/configs/keymaps.lua
+++ .config/nvim/lua/custom/configs/keymaps.lua
@@ -91,3 +91,34 @@ cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'
 nmap <leader>e :e %:h/
 nmap <leader>v :vs %:h/
 ]])
+
+-- Set LSP keymaps
+
+-- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,
+-- 	{ desc = '[R]e[n]ame' })
+-- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action,
+-- 	{ desc = '[C]ode [A]ction' })
+-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition,
+-- 	{ desc = '[G]oto [D]efinition' })
+vim.keymap.set('n', 'gI', vim.lsp.buf.implementation,
+	{ desc = '[G]oto [I]mplementation' })
+vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition,
+	{ desc = 'Type [D]efinition' })
+
+-- See `:help K` for why this keymap
+-- vim.keymap.set('n', 'K', vim.lsp.buf.hover,
+-- 	{ desc = 'Hover Documentation' })
+-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help,
+-- 	{ desc = 'Signature Documentation' })
+
+-- Lesser used LSP functionality
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,
+	{ desc = '[G]oto [D]eclaration' })
+vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder,
+	{ desc = '[W]orkspace [A]dd Folder' })
+vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder,
+	{ desc = '[W]orkspace [R]emove Folder' })
+vim.keymap.set('n', '<leader>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end,
+	{ desc = '[W]orkspace [L]ist Folders' })
