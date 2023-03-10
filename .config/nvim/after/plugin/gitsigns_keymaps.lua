0d3f6c7e3ef287f0b530ab9452f07626db520a58
pavva91 <valerio.mattioli@protonmail.com>
2023-03-10 19:07:52 +0100

    Add Git Signs (GitGutter for nvim) keymaps (]h,[h, <leader>gd,gh,sh,sH])

--- .config/nvim/after/plugin/gitsigns_keymaps.lua
+++ .config/nvim/after/plugin/gitsigns_keymaps.lua
@@ -1,3 +1,7 @@
 -- Git Signs
 vim.keymap.set('n', ']h', '<cmd>Gitsigns next_hunk<cr>', { desc = 'Next [H]unk' })
 vim.keymap.set('n', '[h', '<cmd>Gitsigns prev_hunk<cr>', { desc = 'Previous [H]unk' })
+vim.keymap.set('n', '<leader>gd', '<cmd>Gitsigns diffthis<cr>', { desc = 'Git Diff Open File' })
+vim.keymap.set('n', '<leader>gh', '<cmd>Gitsigns preview_hunk<cr>', { desc = 'Git Diff Open File' })
+vim.keymap.set('n', '<leader>sh', '<cmd>Gitsigns stage_hunk<cr>', { desc = 'Stage Hunk' })
+vim.keymap.set('n', '<leader>sH', '<cmd>Gitsigns undo_stage_hunk<cr>', { desc = 'Undo Stage Hunk' })
