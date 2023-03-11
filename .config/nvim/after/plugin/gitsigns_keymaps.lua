42728e1d959284b9e84a0817443398c4aa20ec1c
pavva91 <valerio.mattioli@protonmail.com>
2023-03-11 13:07:27 +0100

    Add Hunk Select and Hunk Reset keymaps (<leader>hs,<leader>hr)

--- .config/nvim/after/plugin/gitsigns_keymaps.lua
+++ .config/nvim/after/plugin/gitsigns_keymaps.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', '<leader>gd', '<cmd>Gitsigns diffthis<cr>', { desc = '[G]it 
 vim.keymap.set('n', '<leader>gh', '<cmd>Gitsigns preview_hunk<cr>', { desc = '[G]it Preview [H]unk the cursor is on' })
 vim.keymap.set('n', '<leader>sh', '<cmd>Gitsigns stage_hunk<cr>', { desc = '[S]tage [H]unk' })
 vim.keymap.set('n', '<leader>sH', '<cmd>Gitsigns undo_stage_hunk<cr>', { desc = 'Undo [S]tage [H]unk' })
+vim.keymap.set('n', '<leader>hs', '<cmd>Gitsigns select_hunk<cr>', { desc = '[H]unk [S]elect where the cursor is on' })
+vim.keymap.set({ "n", "v" }, '<leader>hr', '<cmd>Gitsigns reset_hunk<cr>',
+{ desc = '[H]unk [R]eset where the cursor is on' })
