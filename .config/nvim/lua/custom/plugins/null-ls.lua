7f40c992ffbfd35c1850ea490e07c2f4a81f9dbe
pavva91 <valerio.mattioli@protonmail.com>
2023-03-20 10:50:04 +0100

    Add Formatting (ESlint, stylua) and gitsigns code actions

--- .config/nvim/lua/custom/plugins/null-ls.lua
+++ .config/nvim/lua/custom/plugins/null-ls.lua
@@ -1,21 +1,30 @@
 return {
-	'jose-elias-alvarez/null-ls.nvim',
+	"jose-elias-alvarez/null-ls.nvim",
 	dependencies = {
-		"nvim-lua/plenary.nvim"
+		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		require("null-ls").setup {
+		require("null-ls").setup({
 			sources = {
 				-- BASH
 				require("null-ls").builtins.formatting.shfmt, -- shell script formatting
 				require("null-ls").builtins.diagnostics.shellcheck, -- shell script diagnostics
 				require("null-ls").builtins.code_actions.shellcheck, -- shell script code actions
 
+				-- FORMATTING
 				-- Prettier
 				require("null-ls").builtins.formatting.prettier, -- markdown formatting
+				require("null-ls").builtins.formatting.stylua, -- lua formatting
 
 				-- ESLint
 				require("null-ls").builtins.code_actions.eslint,
+				require("null-ls").builtins.formatting.eslint,
+
+				-- GitSigns
+				require("null-ls").builtins.code_actions.gitsigns,
+
+				-- Text Lint - The pluggable linting tool for text and Markdown.
+				-- require("null-ls").builtins.diagnostics.textlint
 
 				-- Refactoring (https://github.com/ThePrimeagen/refactoring.nvim)
 				require("null-ls").builtins.code_actions.refactoring.with({
@@ -25,9 +34,11 @@ return {
 				-- Spell suggestions code completion
 				-- require("null-ls").builtins.completion.spell,
 
-			}
-		}
-	end
+				-- Dictionary - show available definition
+				-- require("null-ls").builtins.hover.dictionary,
+			},
+		})
+	end,
 	-- opts = {
 	-- sources = {
 	-- 	require('null-ls').builtins.formatting.shfmt, -- shell script formatting
