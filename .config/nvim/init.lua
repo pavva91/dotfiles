86007b457d371b48e2bc6cc48b34fd2f6dc6033e
pavva91 <valerio.mattioli@protonmail.com>
2023-03-25 17:33:41 +0100

    Fixed bug cmp-cmdline bug: https://github.com/hrsh7th/nvim-cmp/issues/1310

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -53,7 +53,7 @@ require('lazy').setup({
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-cmdline', 'hrsh7th/cmp-path' },
+      'rafamadriz/friendly-snippets', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path' },
   },
 
   -- Useful plugin to show you pending keybinds.
