0dff968aad49068f94c78c70fafba4f4ee2e50f8
pavva91 <valerio.mattioli@protonmail.com>
2023-04-14 12:09:42 +0200

    Fix Mason DAP removed setup_handlers() https://github.com/nvim-lua/kickstart.nvim/pull/258/commits/94ef7a12ed266f5e17385e7250e6826847df347b

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -29,6 +29,9 @@ return {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_setup = true,
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
       -- You'll need to check that you have the required things installed
       -- online, please don't ask me how to install them :)
       ensure_installed = {
@@ -36,10 +39,6 @@ return {
         "delve",
       },
     })
-
-    -- You can provide additional configuration to the handlers,
-    -- see mason-nvim-dap README for more information
-    require("mason-nvim-dap").setup_handlers()
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
