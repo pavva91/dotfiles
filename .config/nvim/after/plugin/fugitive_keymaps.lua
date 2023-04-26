eb419c00fcef03b151decde0716b3caa372a8920
pavva91 <valerio.mattioli@protonmail.com>
2023-04-26 13:22:29 +0200

    Add <leader>gm to solve merge conflicts 3-way diff view (target branch | working copy | merge branch)

--- .config/nvim/after/plugin/fugitive_keymaps.lua
+++ .config/nvim/after/plugin/fugitive_keymaps.lua
@@ -1,2 +1,4 @@
 -- Git
+vim.keymap.set('n', '<leader>gm', '<cmd>Gvdiffsplit!<CR>', { desc = '[G]it [M]erge conflict diff 3-way Vertical Split' })
+vim.keymap.set('n', '<leader>gs', '<cmd>G<CR>', { desc = '[G]it [S]tatus Fugitive' })
 vim.keymap.set('n', '<leader>gg', '<cmd>G log --oneline --decorate --graph --all<CR>', { desc = '[G]it [G]raph Output' })
