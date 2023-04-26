9857a2507bd6740bbd0f4ac24bc89289ebc7b42b
pavva91 <valerio.mattioli@protonmail.com>
2023-04-26 10:42:08 +0200

    Lualine config prettier and formatted

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -1,45 +1,78 @@
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'onedark',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+      theme = "onedark",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
       disabled_filetypes = {},
       always_divide_middle = true,
       globalstatus = false,
       refresh = {
         statusline = 250,
         tabline = 1000,
-      }
+      },
     },
     sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = {
-        { 'filename', path = 1 },
+      lualine_a = {
+        "mode",
       },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' }
+      lualine_b = {
+        "branch",
+        "diff",
+        "diagnostics",
+      },
+      lualine_c = {
+        {
+          "filename",
+          path = 1,
+          file_status = true,
+          symbols = {
+            modified = "[+]", -- Text to show when the file is modified.
+            readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+            unnamed = "[No Name]", -- Text to show for unnamed buffers.
+            newfile = "[New]", -- Text to show for newly created file before first write
+          },
+        },
+      },
+      lualine_x = {
+        "encoding",
+        "fileformat",
+        "filetype",
+      },
+      lualine_y = {
+        "progress",
+      },
+      lualine_z = {
+        "location",
+      },
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
       lualine_c = {
-        { 'filename', path = 1 }
+        {
+          "filename",
+          path = 1,
+        },
       },
-      lualine_x = { 'location' },
+      lualine_x = {
+        "location",
+      },
       lualine_y = {},
-      lualine_z = {}
+      lualine_z = {},
     },
     tabline = {
       lualine_a = {
         -- take all the space
-        { 'tabs', mode = 1, max_length = vim.o.columns }
-      }
+        {
+          "tabs",
+          mode = 1,
+          max_length = vim.o.columns,
+        },
+      },
     },
-    extensions = {}
-  }
+    extensions = {},
+  },
 }
