027629c92460d538c7c3d2bc6b303f6557b221dd
pavva91 <valerio.mattioli@protonmail.com>
2023-04-30 23:23:28 +0200

    Add vim-test plugin

---
+++ .config/nvim/after/plugin/vim-test_keymaps.lua
@@ -0,0 +1,7 @@
+-- Git
+vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>", { desc = "[T]est [T]his function" })
+vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<CR>", { desc = "[T]est [N]earest" })
+vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>", { desc = "[T]est [F]ile" })
+vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<CR>", { desc = "[T]est [S]uite" })
+vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>", { desc = "[T]est [L]ast" })
+vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<CR>", { desc = "[T]est [V]isit" })
