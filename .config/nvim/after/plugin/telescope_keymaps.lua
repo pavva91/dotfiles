7b05ca56a84903ca7cb140d767329dd8937956ef
pavva91 <valerio.mattioli@protonmail.com>
2023-05-01 02:01:13 +0200

    Add Find Git files (fg) and remapped find grep (fG)

--- .config/nvim/after/plugin/telescope_keymaps.lua
+++ .config/nvim/after/plugin/telescope_keymaps.lua
@@ -29,7 +29,7 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
 vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fG', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
 vim.keymap.set('n', '<leader>fj', require('telescope.builtin').jumplist, { desc = '[F]ind [J]ump list' })
 
@@ -41,3 +41,4 @@ vim.keymap.set('n', '<leader>fk', require('telescope.builtin').keymaps, { desc =
 -- Git
 vim.keymap.set('n', '<leader>fs', require('telescope.builtin').git_status, { desc = '[F]ind Git [S]tatus and Diff' })
 vim.keymap.set('n', '<leader>fc', require('telescope.builtin').git_commits, { desc = '[F]ind Git [C]ommits' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files, { desc = '[F]ind [G]it Files (hidden files)' })
