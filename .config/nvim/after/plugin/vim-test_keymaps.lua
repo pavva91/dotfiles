5458c4ae4ed8b79966cb595b6776b14c0d086e4e
pavva91 <valerio.mattioli@protonmail.com>
2023-05-05 10:25:33 +0200

    Add neotest to neovim TDD <leader>tN..

--- .config/nvim/after/plugin/vim-test_keymaps.lua
+++ .config/nvim/after/plugin/vim-test_keymaps.lua
@@ -1,6 +1,7 @@
 -- Git
 vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>", { desc = "[T]est [T]his function" })
 vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<CR>", { desc = "[T]est [N]earest" })
+vim.keymap.set("n", "<leader>tf", "<cmd>TestClass<CR>", { desc = "[T]est [C]lass" })
 vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>", { desc = "[T]est [F]ile" })
 vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<CR>", { desc = "[T]est [S]uite" })
 vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>", { desc = "[T]est [L]ast" })
