8e86e4db8ccc53b99f6d404fd452e585ab9817cc
pavva91 <valerio.mattioli@protonmail.com>
2023-06-27 20:28:14 +0200

    Which Key menu group description Trouble <leader>x

--- .config/nvim/after/plugin/trouble_keymaps.lua
+++ .config/nvim/after/plugin/trouble_keymaps.lua
@@ -1,19 +1,45 @@
 -- Lua
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
-)
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  {silent = true, noremap = true}
-)
+
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+  return
+end
+
+local opts = {
+  mode = "n",    -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil,  -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+
+local mappings = {
+  x = {
+    name = "Trouble",
+    x = {
+      "<cmd>TroubleToggle<cr>",
+      "Trouble Toggle",
+    },
+    w = {
+      "<cmd>TroubleToggle workspace_diagnostics<cr>",
+      "Workspace diagnostics",
+    },
+    d = {
+      "<cmd>TroubleToggle document_diagnostics<cr>",
+      "Document Diagnostics",
+    },
+    l = {
+      "<cmd>TroubleToggle loclist<cr>",
+      "Loc List",
+    },
+    q = {
+      "<cmd>TroubleToggle quickfix<cr>",
+      "Trouble Quickfix",
+    },
+  },
+}
+
+which_key.register(mappings, opts)
+
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
