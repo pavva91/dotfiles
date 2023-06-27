db35137e69b4512ed5ff708aab3f1234790ce523
pavva91 <valerio.mattioli@protonmail.com>
2023-06-27 11:23:35 +0200

    fidget.nvim pin to legacy tag to avoid breaking changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,7 +45,8 @@ require("lazy").setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { "j-hui/fidget.nvim",       opts = {} },
+      -- NOTE: fidget.nvim will soon be completely rewritten. In the meantime, please pin your plugin config to the legacy tag to avoid breaking changes.
+      { "j-hui/fidget.nvim",       tag = "legacy", opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       { "folke/neodev.nvim",       opts = {} },
