7a860c8b93443d8b29a2cb257a492f7f32f291ba
pavva91 <valerio.mattioli@protonmail.com>
2023-06-28 16:48:47 +0200

    Add cmp-dap

---
+++ .config/nvim/lua/custom/plugins/cmp-dap.lua
@@ -0,0 +1,18 @@
+return {
+	"rcarriga/cmp-dap",
+-- 	dependencies = {
+-- 		"hrsh7th/nvim-cmp"
+-- 	},
+-- 	config = function()
+-- 		require("cmp").setup({
+-- 			enabled = function()
+-- 				return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+-- 			end,
+-- 		})
+-- 		require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+-- 			sources = {
+-- 				{ name = "dap" },
+-- 			},
+-- 		})
+-- 	end,
+}
