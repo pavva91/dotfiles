f3a4705e064638879fabae6466470f86e1b60aa8
pavva91 <valerio.mattioli@protonmail.com>
2023-07-29 11:45:47 +0200

    Neotree toggle updated

--- .config/nvim/after/plugin/neotree_keymaps.lua
+++ .config/nvim/after/plugin/neotree_keymaps.lua
@@ -1,3 +1,3 @@
 -- Toggle File System Explorer
-vim.keymap.set('n', '<F3>', "<cmd>NeoTreeShowToggle<cr>", { desc = 'Open File System Explorer' })
-vim.keymap.set('n', '<leader>pt', "<cmd>NeoTreeShowToggle<cr>", { desc = 'Open File System Explorer' })
+vim.keymap.set('n', '<F3>', "<cmd>Neotree toggle<cr>", { desc = 'Open File System Explorer' })
+vim.keymap.set('n', '<leader>pt', "<cmd>Neotree toggle<cr>", { desc = 'Open File System Explorer' })
