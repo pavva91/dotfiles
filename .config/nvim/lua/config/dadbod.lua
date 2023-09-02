fc601cd17dcc06f578637221bcfc1121944b8972
pavva91 <valerio.mattioli@protonmail.com>
2023-09-02 14:10:10 +0200

    Setup autocompletiton Database management (dadbod)

---
+++ .config/nvim/lua/config/dadbod.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+local function db_completion()
+  require("cmp").setup.buffer({
+    sources = {
+      { name = "vim-dadbod-completion" },
+    },
+    formatting = {
+      -- fields = { "menu", "abbr", "kind" },
+      format = function(entry, item)
+        -- fancy icons and a name of kind
+        -- item.kind = require("lspkind").presets.default[item.kind] .. " " .. item.kind
+        item.menu = ({
+          vim_dadbod_completion = "[DB]",
+        })[entry.source.name]
+        -- local menu_icon = {
+        --   vim_dadbod_completion = "[DB]",
+        -- }
+        -- item.menu = menu_icon[entry.source.name]
+        return item
+      end,
+    },
+  })
+end
+
+function M.setup()
+  vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+      "sql",
+    },
+    command = [[setlocal omnifunction=vim_dadbod_completion#omni]],
+  })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+      "sql",
+      "mysql",
+      "plsql",
+    },
+    callback = function()
+      vim.schedule(db_completion)
+    end,
+  })
+end
+
+return M
