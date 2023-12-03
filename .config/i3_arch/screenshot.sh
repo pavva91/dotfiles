c033b3fce080f92c46bd273c1ecfe72f56de4af2
pavva91 <valerio.mattioli@proton.me>
2023-12-03 23:37:10 +0100

    Fix Screenshots

--- .config/i3_arch/screenshot.sh
+++ .config/i3_arch/screenshot.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
-# adapted from: https://legacy.sr.ht/EJ5C.sh
-#output_file="/tmp/screenshot-$(date '+%F-%T').png"
-#output_file="/home/bob/Pictures/Screenshots/screenshot-$(date '+%F-%T').png"
+# INFO: adapted from: https://legacy.sr.ht/EJ5C.sh
+# INFO: output_file="/tmp/screenshot-$(date '+%F-%T').png"
+# INFO: output_file="/home/bob/Pictures/Screenshots/screenshot-$(date '+%F-%T').png"
 output_file="$HOME/Pictures/Screenshots/screenshot-$(date '+%F-%T').png"
-
+[ $# -eq 0 ] && {
+	echo "Usage: $0 needs at least one argument"
+	exit 1
+}
 case "$1" in
-	"full")
-		maim $output_file ;;
-	"rect")
-		maim --select $output_file ;;
-	"active_window")
-		maim --window $(xdotool getactivewindow) $output_file;;
+"full")
+	maim "$output_file"
+	;;
+"rect")
+	maim --select "$output_file"
+	;;
+"active_window")
+	maim --window "$(xdotool getactivewindow)" "$output_file"
+	;;
 esac
