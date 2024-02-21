1cc404a01ad93dff063d82d8772042978dc2b788
pavva91 <valerio.mattioli@proton.me>
2024-02-21 15:20:43 +0100

    fix tmux-sessionizer script to run with 3 use cases: tmux not running, tmux running but not open, tmux running and open session

--- productivity/tmux-sessionizer.sh
+++ productivity/tmux-sessionizer.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash
 
-session=$(find ~ ~/personal ~/work -mindepth 1 -maxdepth 1 -type d | fzf)
-session_name=$(basename "$session" | tr . _)
+selected=$(find ~ ~/gitlab ~/github ~/personal ~/work -mindepth 1 -maxdepth 1 -type d | fzf)
 
-if ! tmux has-session -t "$session_name" 2> /dev/null; then
-    tmux new-session -s "$session_name" -c "$session" -d
+if [[ -z $selected ]]; then
+	exit 0
 fi
 
-tmux switch-client -t "$session_name"
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+# tmux not running
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+	tmux new-session -s $selected_name -c $selected
+	exit 0
+fi
+
+# tmux running but not open
+if [[ -z $TMUX ]] && [[ $tmux_running ]]; then
+	if ! tmux has-session -t=$selected_name 2>/dev/null; then
+		tmux new-session -ds $selected_name -c $selected
+	else
+		tmux attach -t $selected_name
+		exit 0
+	fi
+fi
+
+# tmux running and open
+tmux switch-client -t $selected_name
+
+# if ! tmux has-session -t "$selected_name" 2> /dev/null; then
+#     tmux new-session -s "$selected_name" -c "$selected" -d
+# fi
+#
+# tmux switch-client -t "$selected_name"
