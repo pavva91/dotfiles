e73f093ffd2166611802e46c67cf56a6e1e8e857
pavva91 <valerio.mattioli@proton.me>
2024-06-18 22:50:22 +0200

    clean git script

--- bash/bash_scripts/gitWSLWin10.sh
+++ bash/bash_scripts/gitWSLWin10.sh
@@ -2,11 +2,6 @@
 
 # checks to see if we are in a windows or linux dir
 isInsideMntDir() {
-	# REPO_1=$(pwd | awk -F "/" '{print $1}')
-	# REPO_1=awk -v pwd="$PWD" -F "/" '{print $1}'
-	# echo "$REPO_1"
-	# case $REPO_1 in
-
 	case $PWD/ in
 	/mnt/*)
 		# return "$(true)"
