96817d13cf752bf78c75efd3b9f2cb3658c8d09a
pavva91 <valerio.mattioli@proton.me>
2024-09-05 12:12:05 +0200

    nvim-dap-ui dependency nvim-nio

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -10,7 +10,8 @@ return {
   "mfussenegger/nvim-dap",
   dependencies = {
     -- Creates a beautiful debugger UI
-    "rcarriga/nvim-dap-ui",
+    -- "rcarriga/nvim-dap-ui",
+    { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
 
     -- Installs the debug adapters for you
     "williamboman/mason.nvim",
