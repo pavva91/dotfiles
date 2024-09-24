37aa3c9477beffde579e3c313b48162864208e3b
pavva91 <valerio.mattioli@proton.me>
2024-09-24 02:13:54 +0200

    fix vim-dadbod-cmp autocompletion

--- nvim/.config/nvim/after/plugin/settings.lua
+++ nvim/.config/nvim/after/plugin/settings.lua
@@ -12,7 +12,7 @@ vim.cmd.colorscheme 'tokyonight-night'
 -- Material Style
 vim.g.material_style = 'deep ocean'
 
--- to change run this Ex command: :lua require("material.functions").find_style()
+-- to change run this Ex command: :lua require('material.functions').find_style()
 --
 ---------------------------------------------------------------------------
 -- => FZF https://github.com/junegunn/fzf.vim
@@ -28,3 +28,36 @@ vim.g.fzf_command_prefix = 'Fzf'
 -- => Treesitter
 ---------------------------------------------------------------------------
 vim.treesitter.language.register('html', 'xhtml')
+
+---------------------------------------------------------------------------
+-- => vim-dadbod-completion
+---------------------------------------------------------------------------
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'sql', 'mysql', 'plsql' },
+	callback = function()
+		require('cmp').setup.buffer({
+			sources = {
+				{ name = 'luasnip' },
+				{ name = 'vim-dadbod-completion' },
+				{ name = 'treesitter' },
+			},
+			formatting = {
+				fields = { 'menu', 'abbr', 'kind' },
+				format = function(entry, item)
+					local menu_icon = {
+						luasnip = '[SNIP]',
+						['vim-dadbod-completion'] = ' ',
+						treesitter = '[TS]',
+					}
+
+					item.menu = menu_icon[entry.source.name]
+					return item
+				end,
+			},
+		})
+		vim.schedule(function()
+			print(' DB Chad Mode: ON!')
+		end)
+	end
+})
