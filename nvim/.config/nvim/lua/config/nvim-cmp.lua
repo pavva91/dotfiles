0f9601f6de9740b5ba18bddb1bd45326e5bfa053
pavva91 <valerio.mattioli@proton.me>
2024-10-01 02:16:44 +0200

    clean

--- nvim/.config/nvim/lua/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/config/nvim-cmp.lua
@@ -91,18 +91,6 @@ for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/custom/snippets/*.lu
   loadfile(ft_path)()
 end
 
-vim.keymap.set({ "i", "s" }, "<C-k>", function()
-  if luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
-  end
-end, { silent = true })
-
-vim.keymap.set({ "i", "s" }, "<C-j>", function()
-  if luasnip.jumpable(-1) then
-    luasnip.jump(-1)
-  end
-end, { silent = true })
-
 -- NOTE: Database completion OLD (works)
 -- NOTE: example config https://github.com/kristijanhusak/neovim-config/blob/20cd27b0386acf5106c9faab51bdc6f8b2b3cc10/nvim/lua/partials/completion.lua#L18
 
