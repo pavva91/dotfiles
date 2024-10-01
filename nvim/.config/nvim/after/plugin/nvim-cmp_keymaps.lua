f39a66e01dc37543549dd410574d35bc348fc477
pavva91 <valerio.mattioli@proton.me>
2024-10-01 02:13:45 +0200

    configure nvim-cmp better

---
+++ nvim/.config/nvim/after/plugin/nvim-cmp_keymaps.lua
@@ -0,0 +1,12 @@
+local luasnip = require("luasnip")
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+	if luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	end
+end, { silent = true, desc = "Jump next insertable in the snippet" })
+
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+	if luasnip.jumpable(-1) then
+		luasnip.jump(-1)
+	end
+end, { silent = true, desc = "Jump previous insertable in the snippet" })
--- nvim/.config/nvim/lua/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/config/nvim-cmp.lua
@@ -7,6 +7,7 @@ require("luasnip.loaders.from_vscode").lazy_load()
 luasnip.config.setup({})
 
 cmp.setup({
+  -- Enable luasnip to handle snippet expansion for nvim-cmp
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
@@ -14,6 +15,15 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ["<C-e>"] = cmp.mapping.abort(),
+    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+    ["<C-y>"] = cmp.mapping(
+      cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      },
+      { "i", "c" }
+    ),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete({}),
@@ -44,7 +54,7 @@ cmp.setup({
     { name = "luasnip" },
     { name = "nvim_lsp" },
     { name = "treesitter" },
-    { name = "buffer",               keyword_length = 5 },
+    { name = "buffer",    keyword_length = 5 },
     { name = "path" },
     { name = "nvim_lua" },
     -- { name = "cmp_tabnine",          keyword_length = 5 },
@@ -76,6 +86,23 @@ cmp.setup({
   },
 })
 
+-- NOTE: load custom snippets
+for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/custom/snippets/*.lua", true)) do
+  loadfile(ft_path)()
+end
+
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  if luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  if luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  end
+end, { silent = true })
+
 -- NOTE: Database completion OLD (works)
 -- NOTE: example config https://github.com/kristijanhusak/neovim-config/blob/20cd27b0386acf5106c9faab51bdc6f8b2b3cc10/nvim/lua/partials/completion.lua#L18
 
@@ -98,4 +125,3 @@ cmp.setup({
 -- vim.api.nvim_exec([[
 -- autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
 --   ]], false)
-
