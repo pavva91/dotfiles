4851c8dee055ed7421cbfdf8cb620f793f427005
pavva91 <valerio.mattioli@proton.me>
2024-10-18 09:48:08 +0200

    add treesitter-current-functions plugin

---
+++ nvim/.config/nvim/after/plugin/treesitter-current-functions_keymaps.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n', '<leader>cf', '<cmd>GetCurrentFunctions<CR>', { desc = 'List [C]urrent [F]unctions in the buffer' })
---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-current-functions.lua
@@ -0,0 +1,7 @@
+return {
+	"eckon/treesitter-current-functions",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-telescope/telescope.nvim"
+	},
+}
