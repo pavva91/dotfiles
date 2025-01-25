8a5088107ff2720c12c69533b59e87298eeff70c
pavva91 <valerio.mattioli@proton.me>
2025-01-25 02:35:38 +0100

    fix custom treesitter keymaps based on filetype (js)

---
+++ nvim/.config/nvim/after/plugin/treesitter-textobjects_keymaps.lua
@@ -0,0 +1,53 @@
+-- javascript treesitter keymaps
+vim.api.nvim_create_autocmd("FileType", {
+	-- group = augroup("javascript_treesitter_keymap"),
+	group = vim.api.nvim_create_autocmd("javascript_treesitter_keymaps", { clear = true }),
+	pattern = {
+		"javascript",
+	},
+	callback = function()
+		local ts_textobjects_move = require("nvim-treesitter.textobjects.move")
+
+		vim.keymap.set("n", "]v",
+			function()
+				ts_textobjects_move.goto_next_start("@vardef.outer")
+			end,
+			{ desc = "Next variable definition start" }
+		)
+
+		vim.keymap.set("n", "]{",
+			function()
+				ts_textobjects_move.goto_next_start("@mustache.open")
+			end,
+			{ desc = "Next mustache open" }
+		)
+
+		vim.keymap.set("n", "]}",
+			function()
+				ts_textobjects_move.goto_next_start("@mustache.close")
+			end,
+			{ desc = "Next mustache close" }
+		)
+
+		vim.keymap.set("n", "[v",
+			function()
+				ts_textobjects_move.goto_previous_start("@vardef.outer")
+			end,
+			{ desc = "Previous variable definition start" }
+		)
+
+		vim.keymap.set("n", "[{",
+			function()
+				ts_textobjects_move.goto_previous_start("@mustache.open")
+			end,
+			{ desc = "Previous mustache open" }
+		)
+
+		vim.keymap.set("n", "[}",
+			function()
+				ts_textobjects_move.goto_previous_start("@mustache.close")
+			end,
+			{ desc = "Previous mustache close" }
+		)
+	end,
+})
