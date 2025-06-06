4911e46c3db70d8b5cee1e661295d94dcae99932
pavva91 <valerio.mattioli@proton.me>
2025-06-06 10:16:58 +0200

    fix lsp running twice

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -133,14 +133,14 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Setup mason so it can manage external tooling
-require("mason").setup()
+-- require("mason").setup()
 
 -- -- Ensure the servers above are installed
 local mason_lspconfig = require("mason-lspconfig")
 
-mason_lspconfig.setup({
-  ensure_installed = vim.tbl_keys(servers),
-})
+-- mason_lspconfig.setup({
+--   ensure_installed = vim.tbl_keys(servers),
+-- })
 
 for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
   -- if not excl_servers[server_name] then
